0b8cec740396cd3efdcda94a98a86614331bc527
JongHoonSon <roka1684@gmail.com>
2023-03-01 22:50:35 +0900

    feat: type 지정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-export const Home = () => {
+export const Home = (): React.ReactElement => {
   return <RedBox>Home</RedBox>;
 };
 
