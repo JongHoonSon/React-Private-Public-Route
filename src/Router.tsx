6d0049a9062ebba03e3aad8f46a7ae2bb936934b
JongHoonSon <roka1684@gmail.com>
2023-03-01 22:50:55 +0900

    feat: MyPage 컴포넌트를 PrivateRoute로 감쌈

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import { Home } from "./pages/Home";
 import { MyPage } from "./pages/MyPage";
+import { PrivateRoute } from "./routes/PrivateRoute";
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/mypage" element={<MyPage />} />
+      <Route element={<PrivateRoute />}>
+        <Route path="/mypage" element={<MyPage />} />
+      </Route>
     </Routes>
   );
 };
